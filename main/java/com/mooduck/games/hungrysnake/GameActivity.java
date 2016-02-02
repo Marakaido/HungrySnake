53eed64938f8f8c9e2b52bef4d594cb0cf0eb392
Yaroslav Mykoliv <yaroslav.mykoliv28@gmail.com>
2016-02-02 14:15:02 +0200

    Create an activity for OpenGL ES

--- main/java/com/mooduck/games/hungrysnake/GameActivity.java
+++ main/java/com/mooduck/games/hungrysnake/GameActivity.java
@@ -1,13 +1,21 @@
 package com.mooduck.games.hungrysnake;
 
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends Activity {
+
+    private GLSurfaceView openGLView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        openGLView = new OpenGLSurfaceView(this);
+        setContentView(openGLView);
     }
 }
