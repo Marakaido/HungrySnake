e4816a6e362e076dff980f5fdafa3cd11981f6ba
Yaroslav Mykoliv <yaroslav.mykoliv28@gmail.com>
2016-02-02 18:08:29 +0200

    Create class ShaderProgram

---
+++ main/java/com/mooduck/games/hungrysnake/ShaderProgram.java
@@ -0,0 +1,13 @@
+package com.mooduck.games.hungrysnake;
+
+/**
+ * Created by marakaido on 02.02.16.
+ */
+public class ShaderProgram {
+    private int handle;
+
+    public ShaderProgram(String vertex, String fragment)
+    {
+
+    }
+}
