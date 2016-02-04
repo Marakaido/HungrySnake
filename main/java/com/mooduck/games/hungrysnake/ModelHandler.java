c363b7d17e201fae568ac2ffee7a440e718f8ba5
Yaroslav Mykoliv <yaroslav.mykoliv28@gmail.com>
2016-02-04 15:28:53 +0200

    Modify model
     + Add ModelHandler class
     + Make Model inner class of ModelHandler
     + Add method loadModel

---
+++ main/java/com/mooduck/games/hungrysnake/ModelHandler.java
@@ -0,0 +1,106 @@
+package com.mooduck.games.hungrysnake;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+import java.util.HashMap;
+import java.util.Vector;
+
+/**
+ * Created by marakaido on 02.02.16.
+ */
+
+public class ModelHandler
+{
+    public static class Model
+    {
+        private FloatBuffer VBO;
+        private ShortBuffer indeces;
+
+        private Model() {}
+        private Model(float[] vertices, short[] indeces)
+        {
+            VBO = ByteBuffer.allocateDirect(vertices.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+            VBO.put(vertices);
+            VBO.position(0);
+
+            this.indeces = ByteBuffer.allocateDirect(indeces.length * 2).order(ByteOrder.nativeOrder()).asShortBuffer();
+            this.indeces.put(indeces);
+            this.indeces.position(0);
+        }
+
+        public FloatBuffer getVBO() { return VBO; }
+        public ShortBuffer getIndeces() { return indeces; }
+    }
+
+    private static HashMap<String, Model> models = new HashMap<>();
+
+    // Load model from .obj file into map "models"
+    // Only one object can be loaded at a time
+    public static Model loadModel(Context context, int resourceHandle)
+    {
+        String modelName = "none"; // Name of the model, specified in .obj file
+        Vector<Float> verteces = new Vector<>();
+        Vector<Short> indeces = new Vector<>();
+        try {
+            InputStream inputStream = context.getResources().openRawResource(resourceHandle);
+            BufferedReader fin = new BufferedReader(new InputStreamReader(inputStream));
+            try {
+                String line; // Hold a single line of file
+                String[] data; // Hold parts of line, separated by ' '
+
+                for(line = fin.readLine(); line != null; line = fin.readLine())
+                {
+                    data = line.split(" ");
+                    switch(data[0])
+                    {
+                        case "0": // Set model name
+                            modelName = data[1]; break;
+                        case "v": // Add vertex data
+                            //Log.v("Vertex data", (new Float(data[1])).toString());
+                            for(int i = 1; i < data.length; i++) verteces.add(new Float(data[i]));
+                            break;
+                        case "f": // Add index data
+                            for(int i = 1; i < data.length; i++) indeces.add(new Short(data[i]));
+                            break;
+                        default: continue;
+                    }
+                }
+
+                /* Load data to model */
+                float[] raw_vert = new float[verteces.size()];
+                int i = 0;
+                for(Float val : verteces)
+                {
+                    raw_vert[i] = val;
+                    i++;
+                }
+                short[] raw_ind = new short[indeces.size()];
+                i = 0;
+                for(Short val : indeces)
+                {
+                    raw_ind[i] = val;
+                    i++;
+                }
+                Model result = new Model(raw_vert, raw_ind);
+
+                models.put(modelName, result); // Put new model to map 'models'
+            }
+            finally { fin.close(); }
+        }
+        catch (IOException e)
+        {
+            Log.e("Resource", "failed to load model");
+            e.printStackTrace();
+        }
+        return new Model();
+    }
+}
