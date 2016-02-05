96b2e0a21fc2f15c93e833c5f6a6aeb0f6cf3c2c
Yaroslav Mykoliv <yaroslav.mykoliv28@gmail.com>
2016-02-05 17:47:52 +0200

    - Remove VertexData class  - Remove unnecessary imports

--- main/java/com/mooduck/games/hungrysnake/ShaderHandler.java
+++ main/java/com/mooduck/games/hungrysnake/ShaderHandler.java
@@ -5,12 +5,10 @@ import android.opengl.GLES20;
 import android.util.Log;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Scanner;
 
 /**
  * Created by marakaido on 02.02.16.
